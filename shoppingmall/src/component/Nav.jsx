abf4d482fe4ad5abaecabf8a8043a4dfc279f847
JiinArcade <jn_ll@naver.com>
2023-03-22 14:07:00 +0900

    ✨ style

--- shoppingmall/src/component/Nav.jsx
+++ shoppingmall/src/component/Nav.jsx
@@ -4,16 +4,17 @@ import { faMagnifyingGlass, faBagShopping } from '@fortawesome/free-solid-svg-ic
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const Nav = () => {
-  const loginList = ['매장찾기', '고객센터', '가입하기', '로그인']
-  const menuList = ['New Releases', 'Men', 'Woman', 'Kids', 'Sale', 'SNKRS', '나이키 앱']
+  const loginList = ['매장찾기', '고객센터', '가입하기', '로그인'];
+  const menuList = ['New Releases', 'Men', 'Women', 'Kids', 'Sale', 'SNKRS', '나이키 앱']
+
   return (
     <div>
       <div className='login'>
-        <div className='sub-logo'>
+        <div className='login-img'>
           <img width={25} src="/images/jordan.png" alt="" />
           <img width={30} src="/images/converse.png" alt="" />
         </div>
-        <ul>
+        <ul className='login-list'>
           {
             loginList.map((item, i) => {
               return <li className='login-item' key={i}>{item}</li>
@@ -21,28 +22,31 @@ const Nav = () => {
           }
         </ul>
       </div>
+
       <div className='nav'>
-        <img width={70} src={'/images/nikelogo.png'} alt="nike_logo" />
+        <img width={80} src="/images/nikelogo.png" alt="" />
         <ul className='gnb-list'>
           {
             menuList.map((menu, i) => {
-              return <li className='gnb-tem' key={i}>{menu}</li>
+              return <li className='gnb-item' key={i}>{menu}</li>
             })
           }
         </ul>
+
         <div className='icon-list'>
           <div className='search-box'>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon className='ic-search' icon={faMagnifyingGlass} />
             <input type="text" placeholder='검색' />
           </div>
           <div>
-            <FontAwesomeIcon icon={faHeart} />
-            <FontAwesomeIcon icon={faBagShopping} />
+            <FontAwesomeIcon className='icon' icon={faHeart} />
+            <FontAwesomeIcon className='icon' icon={faBagShopping} />
           </div>
         </div>
       </div>
     </div>
   )
 }
+
 
 export default Nav
